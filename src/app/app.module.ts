d6b84b53bbb4d508e0955e7bed97b278a099d9f3
Irfanul Karim <karimirfanul10@gmail.com>
2020-11-20 00:48:16 +0600

    Search Bus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatListModule} from '@angular/material/list';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSlideToggle, MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material';
+import { FlexLayoutModule} from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { SearchbusComponent } from './searchbus/searchbus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchbusComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatProgressSpinnerModule,
+    MatSliderModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/searchbus/searchbus.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { validateBasis } from '@angular/flex-layout';
+import { District } from '../shared/district';
+
+
+
+
+@Component({
+  selector: 'app-searchbus',
+  templateUrl: './searchbus.component.html',
+  styleUrls: ['./searchbus.component.scss']
+})
+export class SearchbusComponent implements OnInit {
+
+  searchbusForm: FormGroup;
+  district= District;
+
+
+  constructor(private fb: FormBuilder) { 
+    this.createForm();
+  }
+
+  ngOnInit() {
+  }
+
+  createForm(){
+    this.searchbusForm=this.fb.group({
+      from: '',
+      to: '',
+      journeydate: '',
+      returndate: ''
+    });
+  }
+
+}
