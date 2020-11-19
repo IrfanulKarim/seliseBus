eb2d44f6b874a7d5a3cc961c3654eebea2b4b0b1
Irfanul Karim <karimirfanul10@gmail.com>
2020-11-20 01:27:44 +0600

    create json data of buses

---
+++ src/app/shared/bus.ts
@@ -0,0 +1,14 @@
+export class Bus{
+    id: string;
+    name: string;
+    coachno: string;
+    from: string;
+    to: string;
+    status: string;
+    startingPoint: string;
+    endpoint: string;
+    departuretime: string;
+    arrivaltime: string;
+    seatsavailable: string;
+    price: string;
+}
---
+++ src/app/shared/buses.ts
@@ -0,0 +1,49 @@
+import { Bus } from './bus';
+
+export const BUSES: Bus[] =[
+    {
+      id: '1',
+      name: 'FALGUNIMODHUMOTI PARIBAHAN',
+      coachno: '201',
+      from: 'DHK',
+      to: 'KHU',
+      status: 'Non AC',
+      startingPoint: 'Gulistan',
+      endpoint: 'RoyalChattar'
+      departuretime: '6:15 AM',
+      arrivaltime: '11:35 PM',
+      seatsavailable: '36',
+      price: '500.00',
+      
+    },
+    {
+        id: '2',
+        name: 'FALGUNIMODHUMOTI PARIBAHAN',
+        coachno: '203',
+        from: 'DHK',
+        to: 'KHU',
+        status: 'Non AC',
+        startingPoint: 'Gulistan',
+        endpoint: 'RoyalChattar'
+        departuretime: '7:15 AM',
+        arrivaltime: '12:35 PM',
+        seatsavailable: '29',
+        price: '500.00',
+        
+      },
+      {
+        id: '3',
+        name: 'GREEN LINE',
+        coachno: '730',
+        from: 'DHK',
+        to: 'CHI',
+        status: 'AC',
+        startingPoint: 'Razarbagh',
+        endpoint: 'GEC CIRCLE'
+        departuretime: '7:15 AM',
+        arrivaltime: '12:35 PM',
+        seatsavailable: '20',
+        price: '1000.00',
+        
+      },
+];
